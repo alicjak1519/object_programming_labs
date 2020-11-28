4a9293c7bcbd864ddf1cfd22b8a81f5cb1752934
alicjak1519 <akrzeminska1519@gmail.com>
2020-11-28 15:14:59 +0100

    lab7: Add map element interface

---
+++ lab7/src/main/java/MapBoundary.java
@@ -0,0 +1,15 @@
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class MapBoundary implements IPositionChangeObserver {
+
+//    public MapBoundary(){
+//        SortedSet<Vector2d> XSortedObjects = new TreeSet<Vector2d>(XComparator);
+//        SortedSet<Vector2d> YSortedObjects = new TreeSet<Vector2d>(YComparator);
+//    }
+
+    @Override
+    public void positionChanged(Vector2d oldPosition, Vector2d newPosition) {
+
+    }
+}
--- lab7/src/main/java/World.java
+++ lab7/src/main/java/World.java
@@ -4,7 +4,7 @@ public class World {
             MoveDirection[] directions = new OptionsParser().parse(args);
             IWorldMap map = new GrassField(10);
             map.place(new Animal("Squirrel John", map));
-            map.place(new Animal("Squirrel Julia", map, new Vector2d(3, 4)));
+            map.place(new Animal("Squirrel Julia", map, new Vector2d(0, 9)));
 
             System.out.println(map);
             map.run(directions);
